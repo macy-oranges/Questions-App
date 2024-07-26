1a5e153312db4f37eb1fd68095abc505937fe575
Macy <wonganqi888@gmail.com>
2024-07-26 11:26:20 -0700

    Initial Commit

---
+++ Questions App/Questions_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Questions_AppApp.swift
+//  Questions App
+//
+//  Created by Scholar on 7/26/24.
+//
+
+import SwiftUI
+
+@main
+struct Questions_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
